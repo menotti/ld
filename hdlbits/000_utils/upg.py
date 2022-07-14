db130af58fc7fad7b926c948050da1f9ec8d6ab3
Ricardo Menotti <menotti@ufscar.br>
2022-07-14 14:17:02 -0300

    Script ajustado para Grupos e Bancadas (laboratórios)

--- hdlbits/000_utils/upg.py
+++ hdlbits/000_utils/upg.py
@@ -2,10 +2,12 @@
 import os
 files = os.listdir() 
 files.sort()
-prefix = "Group "
+# prefix = "Grupo ##"
+prefix = "Bancada ###"
+last = prefix
 for filename in files:
-    if prefix != filename[:8]:
-        prefix = filename[:8]
+    if last != filename[:len(prefix)]:
+        last = filename[:len(prefix)]
         print("keep:", filename)
     else:
         os.unlink(filename)
