823fe902c4efa629550ac99cb5a0de9898c4ab32
Ricardo Menotti <menotti@gmail.com>
2023-10-23 22:31:21 -0300

    indentation matters

--- digitaljs/basics.v
+++ digitaljs/basics.v
@@ -10,7 +10,8 @@ module basics(
   output o0, o1, 
   output reg o2);
 
-  assign o0 = i0;               // wire
-  assign o1 = i0 & i1 & i2;     // combinational
-  always@(posedge clk) o2 = o1; // sequential (reg)
+  assign o0 = i0;           // wire
+  assign o1 = i0 & i1 & i2; // combinational
+  always@(posedge clk)
+    o2 = o1;                // sequential (reg)
 endmodule
