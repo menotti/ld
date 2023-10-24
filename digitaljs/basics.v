11396f48b92aea78677429ff615c1d311f4665bb
Ricardo Menotti <menotti@gmail.com>
2023-10-23 22:28:56 -0300

    small change

--- digitaljs/basics.v
+++ digitaljs/basics.v
@@ -4,8 +4,13 @@
  * Author: Ricardo Menotti <menotti AT ufscar DOT br>                        * 
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-module basics(input clk, input i0, i1, i2, output o0, o1, output reg o2);
+module basics(
+  input clk, 
+  input i0, i1, i2, 
+  output o0, o1, 
+  output reg o2);
+
   assign o0 = i0;               // wire
-  assign o1 = &{i0,i1,i2};      // combinational (reduction)
+  assign o1 = i0 & i1 & i2;     // combinational
   always@(posedge clk) o2 = o1; // sequential (reg)
 endmodule
