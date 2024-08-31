efbf0fb1e04176d347329be9eca4cec05bc42029
antonioagbernardo <antoniobernardo@estudante.ufscar.br>
2024-08-31 16:25:06 -0300

    Update ripple_carry_adder.v

--- labs/10_time/ripple_carry_adder.v
+++ labs/10_time/ripple_carry_adder.v
@@ -1,31 +1,34 @@
 `timescale 10ns / 10ps
 
-
 module ripple_carry_adder
-  #(parameter WIDTH = 16)
+  #(parameter WIDTH = 8)
 (
-  input [WIDTH-1:0] i_add1,
-  input [WIDTH-1:0] i_add2,
-  input i_cin,
-  output [WIDTH-1:0] o_sum,
-  output o_cout
+  input wire [WIDTH-1:0] i_add1,
+  input wire [WIDTH-1:0] i_add2,
+  output wire [WIDTH:0] o_result
 );
-  wire [WIDTH:0] w_carry;
 
-  assign w_carry[0] = i_cin; // Carry inicial
+  wire [WIDTH-1:0] w_SUM;
+  wire [WIDTH:0] w_C;
 
-  genvar ii;
+  // O carry inicial é 0
+  assign w_C[0] = 1'b0;
+
+  // Gerar os Full Adders
+  genvar jj;
   generate
-    for (ii=0; ii<WIDTH; ii=ii+1) begin: fa
+    for (jj = 0; jj < WIDTH; jj = jj + 1) begin: fa
       full_adder full_adder_inst (
-        .i_bit1(i_add1[ii]),
-        .i_bit2(i_add2[ii]),
-        .i_carry(w_carry[ii]),
-        .o_sum(o_sum[ii]),
-        .o_carry(w_carry[ii+1])
+        .i_bit1(i_add1[jj]),
+        .i_bit2(i_add2[jj]),
+        .i_carry(w_C[jj]),
+        .o_sum(w_SUM[jj]),
+        .o_carry(w_C[jj + 1])
       );
     end
   endgenerate
 
-  assign o_cout = w_carry[WIDTH];
+  // O carry final
+  assign o_result = {w_C[WIDTH], w_SUM};
+
 endmodule
