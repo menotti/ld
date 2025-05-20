9f8962ed520281e03d69c499f1a58968048292f7
mateusdias662 <mateusdias35@estudante.ufscar.br>
2025-05-20 09:24:50 -0300

    Update pos.v

--- labs/07_pos2sop/pos.v
+++ labs/07_pos2sop/pos.v
@@ -1,5 +1,32 @@
+// --- Módulo POS ---
 module pos (
     input x, y, z,
-    output f);
+    output f
+);
     assign f = (x | y | ~z) & (x | ~y | z) & (~x | y | z) & (~x | ~y | ~z);
+endmodule
+
+// --- Módulo SOP ---
+module sop (
+    input x, y, z,
+    output f
+);
+    assign f = (~x & ~y & ~z) |
+               (~x & y & z) |
+               (x & ~y & z) |
+               (x & y & ~z);
+endmodule
+
+// --- Módulo principal TOP ---
+module top (
+    input [2:0] SW,
+    output [9:0] LEDR
+);
+    wire f_pos, f_sop;
+
+    pos p1 (.x(SW[2]), .y(SW[1]), .z(SW[0]), .f(f_pos));
+    sop s1 (.x(SW[2]), .y(SW[1]), .z(SW[0]), .f(f_sop));
+
+    assign LEDR[9:5] = {5{f_pos}};
+    assign LEDR[4:0] = {5{f_sop}};
 endmodule
