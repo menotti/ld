18cc64de95e7f6bae9f353d0c7e4f39a3d761927
Ricardo Menotti <menotti@gmail.com>
2025-06-03 11:07:04 -0300

    fix: template code

--- labs/08_guess/top.v
+++ labs/08_guess/top.v
@@ -1,28 +1,28 @@
 module top(
-  input clk,
+  input CLOCK_50,
   input [9:0] SW,
   output [9:0] LEDR,
   output [6:0] HEX1, HEX0);
 
   wire [6:0] dig1, dig0; // fios para inverter os segmentos
-  integer counter = 0; // contador para piscar
-  always @(posedge clk)
-    counter <= counter + 1;
-    
-  wire shuffle = SW[0]; // embaralhar
-  wire cheat = SW[9]; // responder
-  wire [7:0] random, guess = SW[8:1]; // palpite
-  random r1(clk, shuffle, random); // gera numero aleatorio
-  comp c1(random, guess, match); // compara palpite com numero aleatorio
-
-  // implemente aqui a logica do jogo
-
-
   
-  dec7seg d1(display[7:4], dig1);
-  dec7seg d2(display[3:0], dig0);
-  wire blink = counter[23] & match; // pisca se acertou
-  assign LEDR = {blink, random, blink};
+  integer counter = 0; // contador para piscar
+  always @(posedge CLOCK_50)
+    counter <= counter + 1;
+  assign clk1hz = counter[23];
+
+  wire shuffle = SW[0]; // embaralhar
+  wire [7:0] random, guess = SW[8:1]; // palpite
+
+  // implemente a lógica do jogo aqui
+
+
+
+  dec7seg d1(random[7:4], dig1);
+  dec7seg d0(random[3:0], dig0);
+
+  wire blink = clk1hz & match; // pisca se acertou
+  assign LEDR = {blink, guess, blink};
   assign HEX1 = ~dig1;
   assign HEX0 = ~dig0;
 endmodule
