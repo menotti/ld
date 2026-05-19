574744f46d126c8577ba979f447e4eb7d9e1074c
Ricardo Menotti <menotti@ufscar.br>
2026-05-19 09:53:03 -0300

    Update top.v

--- labs/09_pa/top.v
+++ labs/09_pa/top.v
@@ -9,23 +9,32 @@ R:
  
 */
 
-module top(
-  input sysclk, // 125MHz
-  output [3:0] jb, jc); 
+/* Bancada:
+000000 Subistituir esta linha pelos ras e nomes
 
-  wire [6:0] segs;
-  wire [3:0] val;
-  wire cat;
-  integer count = 0; // 32 bits  
-  always@(posedge sysclk)
-    count = count + 1;
-  //assign clk1hz = count[26]; //≅1Hz https://www.google.com/search?q=125Mhz%2F2^27 
-  //power_on_reset por(sysclk, rst);
+1. Com que frequência este contator conta?
+R: 
 
-  dec7seg dig(val, segs);
+2. Com que frequência os digitos se alternam no display?
+R:
  
-  assign val = count[5] ? count[31:28] : count[27:24];
-  assign cat = count[5]; 
-  assign {jc,jb} = {cat,segs};
+*/
+
+module top(
+  input CLOCK_50, // 50MHz
+  output [6:0] HEX1, HEX0); 
+
+  wire [6:0] segs1, segs0;
+
+  integer count = 0; // 32 bits  
+  always@(posedge CLOCK_50)
+    count = count + 1;
+  //assign clk1hz = count[23]; //≅1Hz https://www.google.com/search?q=50Mhz%2F2^24 
+  //power_on_reset por(CLOCK_50, rst);
+
+  dec7seg dig1(count[31:28], segs1);
+  dec7seg dig0(count[27:24], segs0);
+ 
+  assign {HEX1, HEX0} = ~{segs1, segs0};
 endmodule
 
